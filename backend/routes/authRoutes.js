36d387f06836919a0c395d9ad8c58bfc2067b47e
Octar123 <ranjanassmali123@gmail.com>
2026-05-06 18:10:55 +0530

    created the authMiddleware to check the user is authenticated or not; added the profile route with protect middleware to test;

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { login, register } from '../controllers/authController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/profile', protect, (req, res) => {
+    res.json({
+        message: "You have accessed a protected route!",
+        userData: req.user
+    });
+})
 
 export default router;
