2a9704f4429d7cd6f5491a345b4f9f0ee8bb4f95
Octar123 <ranjanassmali123@gmail.com>
2026-05-06 17:18:47 +0530

    created the authRoutes for /register route; wired it up to the /api/auth route;

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js'
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to the DATABASE"))
