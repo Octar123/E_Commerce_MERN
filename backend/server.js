f78c6e664675dca46108a9bdad0f1effacf7e6c5
Octar123 <ranjanassmali123@gmail.com>
2026-05-08 16:10:41 +0530

    installed and used cors to allow frontend to talk with backend;

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js'
 import productRoutes from  './routes/productRoutes.js'
 import categoryRoutes from  './routes/categoryRoutes.js'
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
